a735ea7e048de3b110d71c30b6e393725dc242af
BalcerzakM <mcj.balcerzak@gmail.com>
2025-05-18 15:21:28 +0200

    karuzela pt2

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
+// AKORDEON
 var akordeon = document.getElementsByClassName("akordeon");
 var i;
 
 for (i = 0; i < akordeon.length; i++) {
-  akordeon[i].addEventListener("click", function() {
-    this.classList.toggle("chosen");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+	akordeon[i].addEventListener("click", function () {
+		this.classList.toggle("chosen");
+		var panel = this.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
 }
+
+//KARUZELA
+
+const slides = document.getElementById("slides");
+const totalSlides = document.querySelectorAll(".slide").length;
+let index = 0;
+
+function showSlide(i) {
+	index = (i + totalSlides) % totalSlides;
+	slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+	showSlide(index + 1);
+}
+
+function prevSlide() {
+	showSlide(index - 1);
+}
+
+setInterval(() => {
+	nextSlide();
+}, 7000);
