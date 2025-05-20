daef77e38186cec026a36a4d3648f00411f32402
pborysq <pborysq@gmail.com>
2025-05-20 22:40:09 +0200

    readme i css do diets

--- list.php
+++ list.php
@@ -5,7 +5,7 @@
         if ($result->num_rows > 0) {
             while($row = mysqli_fetch_row($result)) {
                 echo '<div class="single-product">';
-                echo '    <img src="images\'' . $row[6] . '" class="product-image" alt="'.$row[6].'">';
+                echo '    <img src="images/' . $row[6] . '" class="product-image" alt="'.$row[6].'">';
                 echo '    <div class="product-body">';
                 echo '      <h5>' . $row[1] . '</h5>';
                 echo '      <p><strong>Kaloryczność:</strong> ' . $row[3] . ' kcal</p>';
