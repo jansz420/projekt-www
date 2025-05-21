04fef5a1ac543a1c53a5dcc2be55fd60dc2713b2
jansz <91958@student.pb.edu.pl>
2025-05-21 18:54:24 +0200

    dropdown menu, poprawa script.js, poprawa loga

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 var akordeon = document.getElementsByClassName("akordeon");
 var i;
 
-for (i = 0; i < akordeon.length; i++) {
-	akordeon[i].addEventListener("click", function () {
-		this.classList.toggle("chosen");
-		var panel = this.nextElementSibling;
-		if (panel.style.maxHeight) {
-			panel.style.maxHeight = null;
-		} else {
-			panel.style.maxHeight = panel.scrollHeight + "px";
-		}
-	});
+if (akordeon) {
+	for (i = 0; i < akordeon.length; i++) {
+		akordeon[i].addEventListener("click", function () {
+			this.classList.toggle("chosen");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight) {
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			}
+		});
+	}
 }
 
 //KARUZELA
@@ -20,37 +22,56 @@ const slides = document.getElementById("slides");
 const totalSlides = document.querySelectorAll(".slide").length;
 let index = 0;
 
-function showSlide(i) {
-	index = (i + totalSlides) % totalSlides;
-	slides.style.transform = `translateX(-${index * 100}%)`;
+if (slides && totalSlides) {
+	function showSlide(i) {
+		index = (i + totalSlides) % totalSlides;
+		slides.style.transform = `translateX(-${index * 100}%)`;
+	}
+	
+	function nextSlide() {
+		showSlide(index + 1);
+	}
+	
+	function prevSlide() {
+		showSlide(index - 1);
+	}
+	
+	setInterval(() => {
+		nextSlide();
+	}, 7000);
 }
 
-function nextSlide() {
-	showSlide(index + 1);
-}
-
-function prevSlide() {
-	showSlide(index - 1);
-}
-
-setInterval(() => {
-	nextSlide();
-}, 7000);
 
 // KONTAKT SUBMIT
 
 const contactForm = document.getElementById("contact-form");
 const submitOverlay = document.getElementById("overlay");
 
-contactForm.addEventListener("submit", function(showOverlay) {
-	showOverlay.preventDefault();
-
-	submitOverlay.classList.add("active-overlay");
-
-	document.body.style.cursor = "progress";
-	setTimeout(function() {
-		contactForm.submit();
-		document.body.style.cursor = "default";
-	}, 4000);
+if (contactForm && submitOverlay) {
+	contactForm.addEventListener("submit", function(showOverlay) {
+		showOverlay.preventDefault();
 	
-});
+		submitOverlay.classList.add("active-overlay");
+	
+		document.body.style.cursor = "progress";
+		setTimeout(function() {
+			contactForm.submit();
+			document.body.style.cursor = "default";
+		}, 4000);
+		
+	});
+}
+
+
+// DROPDOWN MENU
+
+const toggle = document.getElementById("dropdown-toggle");
+const menu = document.getElementById("default-menu");
+
+if (toggle && menu) {
+	toggle.addEventListener("click", function () {
+		menu.classList.toggle("show");
+		toggle.classList.toggle("open");
+	});
+}
+
